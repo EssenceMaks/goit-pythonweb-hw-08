39905b9182b94671383e49cc2ce31b71533aa920
EssenceMaks <makski.wish@gmail.com>
2025-04-25 19:51:09 +0300

    main deploy

--- create_db.py
+++ create_db.py
@@ -1,9 +1,14 @@
 import psycopg2
 
-DB_NAME = 'contacts_db'
-DB_USER = 'postgres'
-DB_PASSWORD = '303010'
-DB_HOST = 'localhost'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
 
 conn = psycopg2.connect(dbname='postgres', user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 conn.autocommit = True
